f48db4f35c0511dcf47bd700ab90f036c234b890
Nareshn99 <nareshngohil99@gmail.com>
2022-09-19 20:31:02 +0530

    Frontend connected

--- src/index.js
+++ src/index.js
@@ -14,10 +14,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://dishapushpad1997:0DproZ5drs9XMYYT@group31database.m3wg7re.mongodb.net/college_interns?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
+
     .then(() => console.log("MongoDb is connected......."))
     .catch(err => console.log(err))
 
+
+
 app.use('/', route);
+
 
 
 app.listen(process.env.PORT || 3001, function () {
